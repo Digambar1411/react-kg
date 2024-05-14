e30d597d3a37298e375cd02fc0fa9bc1f1729504
digambar1411 <deshawaldigambar@gmail.com>
2024-05-14 15:29:36 +0530

    wrap app inside provider

--- projects/social-media/src/main.jsx
+++ projects/social-media/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { PostContextProvider } from './store/post-context.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PostContextProvider>
+      <App />
+    </PostContextProvider>
   </React.StrictMode>,
 )
