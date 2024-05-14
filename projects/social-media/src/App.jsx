5ed3690d06b156ab71931da796e4c3e813f569cd
digambar1411 <deshawaldigambar@gmail.com>
2024-05-14 15:40:16 +0530

    setup

--- projects/social-media/src/App.jsx
+++ projects/social-media/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
+import { AddPost } from './components/AddPost';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { PostList } from './components/PostList';
+import { Sidebar } from './components/Sidebar';
+import { usePost } from './store/post-context'
 
 function App() {
 
+  const { currentTab } = usePost();
+
   return (
-    <>
-      
-    </>
+    <div className='social-media'>
+      <div>
+        <Sidebar />
+      </div>
+      <div className='right-section'>
+        <Header />
+        {currentTab === 'Home' ? <AddPost /> : <PostList />}
+        <Footer />
+      </div>
+    </div>
   )
 }
 
