596529baaaed71666c669f5c56d8b652f4d83db3
digambar1411 <deshawaldigambar@gmail.com>
2024-05-20 12:35:07 +0530

    remove strict mode

--- projects/social-media/src/main.jsx
+++ projects/social-media/src/main.jsx
@@ -5,9 +5,9 @@ import { PostContextProvider } from './store/post-context.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <PostContextProvider>
       <App />
     </PostContextProvider>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
